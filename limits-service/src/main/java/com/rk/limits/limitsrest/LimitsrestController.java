c3a15f14ecd5736a3d3045a12a14ac6f80bc9cf2
RMattapalli <RMattapalli@HIBAML134346.ad.harman.com>
2020-02-01 18:02:23 +0530

    Hystrix implemented  (to avoid the fault-tolerance in microservice)

--- limits-service/src/main/java/com/rk/limits/limitsrest/LimitsrestController.java
+++ limits-service/src/main/java/com/rk/limits/limitsrest/LimitsrestController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.rk.limits.configurationproperties.Configuration;
 import com.rk.limits.model.LimitConfiguration;
 
@@ -12,10 +13,26 @@ import com.rk.limits.model.LimitConfiguration;
 public class LimitsrestController {
 	@Autowired
 	Configuration configuration;
-	
- @GetMapping("/limits")
- public LimitConfiguration retrievelLimitsFromConfigurations() {
-	 return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimun());
- }
- 
+
+	@GetMapping("/limits")
+	public LimitConfiguration retrievelLimitsFromConfigurations() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimun());
+	}
+
+	@GetMapping("/fault-tolerance-example")
+
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+
+	public LimitConfiguration retrieveConfiguration() {
+
+		throw new RuntimeException("Not available");
+
+	}
+
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+
+		return new LimitConfiguration(02, 2);
+
+	}
+
 }
